2389e22cca22afe2949367c39d8ad52a0240cc55
Maitlend <Maitlend.merrill@gmail.com>
2023-02-27 15:23:25 -0500

    Implement first version of playerFactory Object

--- script.js
+++ script.js
@@ -17,9 +17,46 @@ const gameboard = (() => {
   return {playerMove, viewGameboard};
 })();
 
+const playerFactory = (name) => {
+  const getName = () => name;
+
+  function makeMove(position){
+    gameboard.playerMove(name, position);
+  }
+
+  if(name === 'X' || name === 'O'){
+    return {getName, makeMove}
+  }
+  else{
+    console.log(`ERROR: player name = ${name} but must be 'X' or 'O'`);
+  }
+}
+
+const displayController = (() => {
+
+})();
+
+
 // Test gameboard functionality
-gameboard.viewGameboard(); 
-gameboard.playerMove('X',0);
+// gameboard.viewGameboard(); 
+// gameboard.playerMove('X',0);
+// gameboard.viewGameboard();
+// gameboard.playerMove('O',1);
+// gameboard.viewGameboard();
+
+// Test playerfactory() 
+const playerO = playerFactory("O");
+const playerX = playerFactory("X");
+
+console.log(playerO.getName());
+console.log(playerX.getName());
+
+// const invalidPlayer = playerFactory("A");
+
+playerO.makeMove(0);
+playerX.makeMove(1);
 gameboard.viewGameboard();
-gameboard.playerMove('O',1);
-gameboard.viewGameboard();
+
+
+
+
